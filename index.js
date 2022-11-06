8075ce60f9a27ec05aad032cbb668dcd77bc10e3
Steven Krenn <stevekrenn@gmail.com>
2022-11-06 16:41:43 -0600

    added MANAGER template in index.js for inquirer

--- index.js
+++ index.js
@@ -5,10 +5,60 @@ const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const generateHTML = require('./src/generateHTML')
 
-//Inquirer for Mangager ?'s:
+//Inquirer for MANAGER ?'s:
+//name, id, email, officeNumber
+const inqManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter MANAGER name:',
+
+            type: 'input',
+            name: 'id',
+            message: "Enter the MANAGER\'S ID #:",
+
+            type: 'input',
+            name: 'email',
+            message: 'Enter the MANAGER\'S e-mail:',
+
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter the MANAGER\'S office number:'
+        }
+    ])
+}
+
 
 //Inquirer for Engineer ?'s:
+//name, id, email, github
 
 //Inquirer for Intern ?'s:
+//name, id, email, school
 
+//Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project\'s name?',
+        name: 'title'
+    },
+];
 
+//Write INDEX.HTML file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(
+        `./dist/${fileName}`, data)
+}
+
+//Initialize app
+function init() {
+    return inquirer.prompt(questions)
+        .then((response) => {
+            console.log(response)
+            writeToFile('index.html', generateHTML(response))
+        })
+}
+
+// Function call to initialize app
+init();
