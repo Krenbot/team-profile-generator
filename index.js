536598e317f6ea95239e7cc7848023afe874625e
Steven Krenn <stevekrenn@gmail.com>
2022-11-12 08:40:30 -0600

    raised function promptChoices

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer')
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
-// const generateHTML = require('./src/generateHTML')
+const generateHTML = require('./src/generateHTML')
 
 let employees = [];
 
@@ -32,6 +32,27 @@ const inqManager = () => {
             let manager = new Manager(info.name, info.id, info.email, info.officeNumber)
             employees.push(manager)
             promptChoices()
+        })
+}
+
+//Inquirer Prompt for adding more employees
+const promptChoices = () => {
+    return inquirer.prompt([{
+        type: 'list',
+        message: 'What would you like to add?',
+        name: 'choices',
+        choices: ['Intern', 'Engineer', 'EXIT']
+    }
+    ])
+        .then((info) => {
+            console.log(info)
+            if (info.choices === 'Intern') {
+                inqIntern()
+            } else if (info.choices === 'Engineer') {
+                inqEngineer();
+            } else {
+                return
+            }
         })
 }
 
@@ -91,27 +112,6 @@ const inqIntern = () => {
         })
 }
 
-//Inquirer Prompt for adding more employees
-const promptChoices = () => {
-    return inquirer.prompt([{
-        type: 'list',
-        message: 'What would you like to add?',
-        name: 'choices',
-        choices: ['Intern', 'Engineer', 'EXIT']
-    }
-    ])
-        .then((info) => {
-            console.log(info)
-            if (info.choices === 'Intern') {
-                inqIntern()
-            } else if (info.choices === 'Engineer') {
-                inqEngineer();
-            } else {
-                return
-            }
-        })
-}
-
 function writeToFile(data) {
     fs.writeFileSync(
         `./dist/index.html`, data, (err) => {
@@ -121,6 +121,4 @@ function writeToFile(data) {
 
 // Function call to initialize app
 inqManager();
-inqEngineer();
-inqIntern();
 writeToFile();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -5,7 +5,7 @@ const generateManager = function (employees) {
         <h4><i class="fas fa-coffee"></i>Manager</h4>
     </div>
     <div class="card-body">
-        <p>ID: ${manager.id}</p>
+        <p>ID: ${employees.manager.id}</p>
         <p>Email: <a href="mailto:${employees.manager.email}">${employees.manager.email}</a></p>
         <p>Office Number:${employees.manager.officeNumber}</p>
     </div>
@@ -29,15 +29,19 @@ const generateEngineer = function (employees) {
 const generateIntern = function (employees) {
     return `<article class="card col-3 m-3">
     <div class="card-header background">
-        <h2>${inter.name}</h2>
+        <h2>${employee.intern.name}</h2>
         <h4><i class="fa-solid fa-user-graduate"></i>Intern</h4>
     </div>
     <div class="card-body">
         <p>ID: ${employees.intern.id}</p>
-        <p>Email: <a href="mailto:${employees.intern.email}">${employees.intern.emai.}</a></p>
+        <p>Email: <a href="mailto:${employees.intern.email}">${employees.intern.email}</a></p>
         <p>School: ${employees.intern.school}</p>
     </div>
 </article>`
 }
 
-module.exports = generateHTML
+generateManager();
+generateEngineer();
+generateIntern();
+
+// module.exports = generateHTML
